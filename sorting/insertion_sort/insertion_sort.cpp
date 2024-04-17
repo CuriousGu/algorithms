48e2e625cee0702130b6827e8f27a3f22352384d
CuriousGu <gustavo_ortega@usp.br>
2024-04-16 23:14:16 -0300

    insertion sort - cpp

---
+++ sorting/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+using namespace std; 
+
+
+vector<int> sort(vector<int> arr){
+
+    for(int i=1; i < arr.size(); i++){
+        int temp = arr[i]; 
+        int j = i - 1; 
+        
+        while(j >= 0 && arr[j] > temp){
+            arr[j+1] = arr[j]; 
+            j--; 
+        }
+        arr[j+1] = temp; 
+    }
+    return arr; 
+}
